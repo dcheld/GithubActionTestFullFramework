873bc62624b4822db7dd08891587eafaee6fc603
Douglas de Campos Held <douglas.held@ciandt.com>
2023-02-22 15:25:49 -0300

    Segundo projeto de teste

---
+++ test/Project2UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Project2UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Ci&T Software")]
+[assembly: AssemblyProduct("Project2UnitTest")]
+[assembly: AssemblyCopyright("Copyright © Ci&T Software 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e81b4878-7ccd-4049-991e-ca5b44af1b7f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ test/Project2UnitTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Project2UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Project2UnitTest_TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
